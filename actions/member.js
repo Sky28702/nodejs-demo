ea628dcf2f1175f880a28fa5db6b35c113dc4509
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-07-12 01:14:07 +0530

    more folder fixtures

--- actions/member.js
+++ actions/member.js
@@ -1,2 +1,20 @@
 async function createMember(req, res) {}
 export { createMember };
+
+// "use server";
+// import { Member } from "../models/memberModel";
+
+// import dbConnect from "@/app/backend/db/dbConnect";
+
+// async function createMember(data) {
+//   await dbConnect();
+//   const member = new Member({
+//     memberName: data.memberName,
+//     gender: data.gender,
+//   });
+//   await member.save();
+
+//   console.log(member);
+//   return member.toString();
+// }
+// export { createMember };
