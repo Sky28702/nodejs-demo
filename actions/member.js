0c4c7f004d94bac3562d02070a4c32fb9968c554
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-07-14 02:43:41 +0530

    CRUD DONE

--- actions/member.js
+++ actions/member.js
@@ -19,24 +19,70 @@ async function createMember(req, res) {
     console.log(error);
   }
 }
-export { createMember };
 
-// "use server";
-// import { Member } from "../models/memberModel";
+async function getMembers(req, res) {
+  try {
+    const allMember = await Member.find({}).sort({ createdAt: -1 });
 
-// import dbConnect from "@/app/backend/db/dbConnect";
+    res.send({
+      data: allMember,
+      success: true,
+      message: "here is list if every member",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-// async function createMember(data) {
-//   await dbConnect();
-//   const member = new Member({
-//     memberName: data.memberName,
-//     gender: data.gender,
-//   });
-//   await member.save();
+async function memberView(req, res) {
+  try {
+    const { id } = req.params;
+    const memberView = await Member.findById(id);
+    res.send({
+      data: memberView,
+      success: true,
+      message: "Member is here",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-//   console.log(member);
-//   return member.toString();
+async function deleteMember(req, res) {
+  try {
+    const { id } = req.params;
+    await Member.findByIdAndDelete(id);
+
+    res.send({
+      success: true,
+      message: "Member is deleted",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function updateMember(req, res) {
+  let member = {
+    memberName: req.body.memberName,
+    gender: req.body.gender,
+  };
+  try {
+    const { id } = req.params;
+    const updatedMember = await Member.findByIdAndUpdate(id, member);
+    res.send({
+      data: updateMember,
+      success: true,
+      message: "data is updated",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { createMember, getMembers, deleteMember, memberView, updateMember };
+
+// async function deleteItem(id) {
+//   await Product.findByIdAndDelete(id);
+//   console.log(`Item ${id} deleted`);
+//   return deleteItem;
 // }
-// export { createMember };
-
-// these are just for reading and references....
--- routes/member.js
+++ routes/member.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import { createMember } from "../actions/member.js";
+import { getMembers } from "../actions/member.js";
+import { deleteMember } from "../actions/member.js";
+import { memberView } from "../actions/member.js";
+import { updateMember } from "../actions/member.js";
 
 const memberRouter = Router();
 memberRouter.post("/members/create", createMember);
+memberRouter.get("/members/all", getMembers);
+memberRouter.get("/members/view/:id", memberView);
+memberRouter.delete("/members/delete/:id", deleteMember);
+memberRouter.put("/members/update/:id", updateMember);
 export default memberRouter;
